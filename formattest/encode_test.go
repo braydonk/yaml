f5a7b1b04fc2f012f3793c45d0c0eda0fadcc6c5
braydonk <braydonk@google.com>
2023-03-14 20:39:18 -0400

    add formatting test suites

---
+++ formattest/encode_test.go
@@ -0,0 +1,53 @@
+package formattest
+
+import (
+	"testing"
+
+	"github.com/braydonk/yaml"
+)
+
+func TestExplicitDocumentStart(t *testing.T) {
+	formatTestCase{
+		name:             "explicit document start",
+		folder:           "document_start",
+		configureDecoder: noopDecoder,
+		configureEncoder: func(enc *yaml.Encoder) {
+			enc.SetExplicitDocumentStart(true)
+		},
+	}.Run(t)
+}
+
+func TestIndentless(t *testing.T) {
+	formatTestCase{
+		name:             "indentless array",
+		folder:           "indentless",
+		configureDecoder: noopDecoder,
+		configureEncoder: func(enc *yaml.Encoder) {
+			enc.SetIndentlessBlockSequence(true)
+		},
+	}.Run(t)
+}
+
+func TestIndentedToIndentless(t *testing.T) {
+	formatTestCase{
+		name:             "indented to indentless array",
+		folder:           "indented_to_indentless",
+		configureDecoder: noopDecoder,
+		configureEncoder: func(enc *yaml.Encoder) {
+			enc.SetIndentlessBlockSequence(true)
+		},
+	}.Run(t)
+}
+
+func TestBlockScalar(t *testing.T) {
+	formatTestCase{
+		name:   "block scalar decoding and encoding",
+		folder: "block_scalar",
+		configureDecoder: func(dec *yaml.Decoder) {
+			dec.SetScanBlockScalarAsLiteral(true)
+		},
+		configureEncoder: func(enc *yaml.Encoder) {
+			enc.SetAssumeBlockAsLiteral(true)
+		},
+	}.Run(t)
+}
--- test/README.md
+++
@@ -1,3 +0,0 @@
-# Fork Tests
-
-These are where new tests written for this fork will go. The custom test framework this repo sets up is not really my thing, so I'm sticking to standard library tests when I write anything new.
